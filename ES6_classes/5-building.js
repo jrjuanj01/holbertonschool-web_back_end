b369f79dccb22b54fc691cd74460759d99fc0486
jrjuanj01 <jrjuanj01@gmail.com>
2024-07-18 15:56:45 -0400

    Fixed the error message

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -4,18 +4,12 @@ export default class Building {
      */
   constructor(sqft) {
     this._sqft = sqft;
-    if (new.target === Building) {
-      throw new Error('Cannot instantiate abstract class Building directly');
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
 
   get sqft() {
     return this._sqft;
   }
-
-  /* eslint-disable */
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
-  /* eslint-enable */
 }
