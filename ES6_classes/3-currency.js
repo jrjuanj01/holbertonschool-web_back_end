a70cd3cdb788b489f0a740f3ce39f39f2b2d3c90
jrjuanj01 <jrjuanj01@gmail.com>
2024-07-18 15:25:21 -0400

    Added file 3

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,36 @@
+export default class Currency {
+  /**
+   * @param {string} code
+   * @param {string} name
+   */
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Code must be a string');
+    }
+    this._code = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
