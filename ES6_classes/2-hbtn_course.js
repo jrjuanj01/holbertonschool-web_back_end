09ae70cb24cbacfa831f4e4c4f4e0145261d3ef9
jrjuanj01 <jrjuanj01@gmail.com>
2024-07-18 14:59:02 -0400

    Added file 2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,5 +1,16 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (name !== 'string') {
+      throw new Error('Name must be a string');
+    }
+
+    if (typeof length !== 'number') {
+      throw new Error('Length must be a number');
+    }
+
+    if (!Array.isArray(students)) {
+      throw new Error('Students must be an array');
+    }
     this._name = name;
     this._length = length;
     this._students = students;
