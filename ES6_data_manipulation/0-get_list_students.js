3ed23fedc8e0875c90217ad1c80789e9d377f77c
jrjuanj01 <jrjuanj01@gmail.com>
2024-07-22 11:35:29 -0400

    Added file 0

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  const listStudents = [];
+  listStudents.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
+  listStudents.push({ id: 2, firstName: 'James', location: 'Columbia' });
+  listStudents.push({ id: 5, firstName: 'Serena', location: 'San Francisco' });
+  return listStudents;
+}
