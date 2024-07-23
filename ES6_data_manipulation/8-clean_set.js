600944670c44aabecf06ab263099517d5015c5ae
jrjuanj01 <jrjuanj01@gmail.com>
2024-07-23 12:23:06 -0400

    fixed typo

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -4,6 +4,6 @@ export default function cleanSet(set, startString) {
   }
   return Array.from(set)
     .filter((item) => item.startsWith(startString))
-    .map((item) => item.slice(startString.legth))
+    .map((item) => item.slice(startString.length))
     .join('-');
 }
