3ec3b0fb6a7e585a08be442bf89a6b2142c3264c
jrjuanj01 <jrjuanj01@gmail.com>
2024-07-24 15:58:48 -0400

    Attempt # 2

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,3 +1,5 @@
 export default function uploadPhoto(fileName) {
-  return new Promise.reject(() => new Error(`${fileName} cannot be processed`));
+  return new Promise((resolve, reject) => {
+    reject(new Error(`${fileName} cannot be processed`));
+  });
 }
