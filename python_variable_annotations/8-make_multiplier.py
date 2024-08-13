474aea063b6dd2a4c4cc44967a53f0884f43bd13
jrjuanj01 <jrjuanj01@gmail.com>
2024-08-13 13:18:19 -0400

    Fixed pycodestyle

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -2,11 +2,12 @@
 """file tha tcontains the function make_multiplier"""
 from typing import Callable
 
-def make_multiplier(multiplier:float) -> Callable[[float], float]:
-    """Function that returns a new function that multiplies its input by a given multiplier"""
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Function that returns a function that multiplies its input"""
 
     def multiplier_func(number: float) -> float:
-        """Inner function that multiplies the input number by the given multiplier"""
+        """Inner function that multiplies the input by multiplier"""
 
         return number * multiplier
 
