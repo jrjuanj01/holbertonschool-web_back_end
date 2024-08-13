72686883e47a013b1e1ae789f0646b56289182ab
jrjuanj01 <jrjuanj01@gmail.com>
2024-08-13 12:43:30 -0400

    Added task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""File that contains the function floor"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Function that returns the floor of a float"""
+
+    return math.floor(n)
