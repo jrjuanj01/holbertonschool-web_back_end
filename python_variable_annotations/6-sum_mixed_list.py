b38b6e3be8abdd39a2e3dc671871539af28df684
jrjuanj01 <jrjuanj01@gmail.com>
2024-08-13 12:58:38 -0400

    Attempt #2

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -2,6 +2,7 @@
 """File that contains the function sum_mixed_list """
 from typing import List, Union
 
+
 def sum_mixed_list(lst: List[Union[int, float]]) -> float:
     """Function that returns the sum of all elements in a list"""
 
