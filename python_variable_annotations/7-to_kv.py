cba6f2a085f04cedbff4bbb77cf3fb99da899de5
jrjuanj01 <jrjuanj01@gmail.com>
2024-08-13 13:01:50 -0400

    Added task #7

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""File that contains the function to_kv"""
+
+
+def to_kv(k: str, v:float) -> tuple:
+    """Function to convert a key-value pair to a tuple"""
+
+    return (k, v)
