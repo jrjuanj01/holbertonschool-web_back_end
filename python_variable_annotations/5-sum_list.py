07e70f1a066bbefdf8b52ad6339410bae9291c71
jrjuanj01 <jrjuanj01@gmail.com>
2024-08-13 12:54:35 -0400

    Added task #5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""File that contains the function sum_list"""
+
+
+def sum_list(input_list: list) -> float:
+    """Function that returns the sum of all elements in a list"""
+
+    return sum(input_list)
