c0b35379f8fe4a6774a58eb739190415b5301a33
jrjuanj01 <jrjuanj01@gmail.com>
2024-08-13 13:12:19 -0400

    Attempt #5

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -2,7 +2,8 @@
 """File that contains the function to_kv"""
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Function to convert a key-value pair to a tuple"""
 
-    return (k, v^(2))
+    return (k, v ** 2)
