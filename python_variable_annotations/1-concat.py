567be66b14cf5e81ebf8a1247845ec9212c81b43
jrjuanj01 <jrjuanj01@gmail.com>
2024-08-13 12:41:01 -0400

    Added task #1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/ env python3
+"""File that cpntains the function strconcat"""
+
+
+def strconcat(str1: str, str2: str) -> str:
+    """Function to concatenate two strings"""
+
+    return str1 + str2
