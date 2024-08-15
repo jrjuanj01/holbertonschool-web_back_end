94d3badfbb7937ba723e5bd1a932d3c061a65dba
jrjuanj01 <jrjuanj01@gmail.com>
2024-08-15 12:28:48 -0400

    Switched import order

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """File that contains a measure time function"""
-wait_n = __import__('1-concurrent_coroutines').wait_n
 import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 async def measure_time(n: int, max_delay: int) -> float:
