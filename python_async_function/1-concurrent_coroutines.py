b6aee5c7ad4b3653bd0e22bf8a73786ca2a1f808
jrjuanj01 <jrjuanj01@gmail.com>
2024-08-15 12:21:28 -0400

    Attempt #8

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,20 +4,20 @@ from typing import List
 wR = __import__('0-basic_async_syntax').wait_random
 
 
-def bubble_sort(arr: List):
-    """Function that sorts an array using bubble sort"""
-    n = len(arr)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-
-
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Function that waits for n random times and sorts the result"""
-    result = []
-    for i in range(n):
-        wait_time = await wR(max_delay)
-        result.append(wait_time)
-        result = bubble_sort(result)
-    return result
+    """A function that waits for n random times"""
+    delays = []
+
+    for x in range(n):
+        delay = await wR(max_delay)
+        delays.append(delay)
+
+    for i in range(1, len(delays)):
+        key = delays[i]
+        j = i - 1
+        while j >= 0 and key < delays[j]:
+            delays[j + 1] = delays[j]
+            j -= 1
+        delays[j + 1] = key
+
+    return delays
