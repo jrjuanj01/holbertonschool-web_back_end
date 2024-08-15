95aea70dad5ce4d74ef0fa3c6ad7d9a1e16174d1
jrjuanj01 <jrjuanj01@gmail.com>
2024-08-15 11:30:31 -0400

    Added task #1

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3 
+"""A file that contains a wait"""
+import asyncio
+from typing import List
+
+
+wR = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int, max_delay: int) -> List:
+    result = []
+    for i in range(n):
+        wait_time = await wR(max_delay)
+        result.append(wait_time)
+    return sorted(result)
+    
