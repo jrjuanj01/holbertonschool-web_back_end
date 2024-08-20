a292191f638326bd533cd4204e5bd42e408ff346
jrjuanj01 <jrjuanj01@gmail.com>
2024-08-20 12:39:43 -0400

    fixed pycodestyle

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -5,5 +5,5 @@ from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple:
     """function that returns the the index and size of a page"""
-    
+
     return (((page * page_size) - page_size), (page * page_size))
