791de31bffff1ceb7cb16d5118c9fe78610308cf
jrjuanj01 <jrjuanj01@gmail.com>
2024-08-20 11:23:44 -0400

    checkpoint

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""File that contains the function index range"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+    """function that returns the the index and size of a page"""
+    
+    return((),())
