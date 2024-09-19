80ccd60e8128f3af7969e5ba89ca2df72f5f84f9
jrjuanj01 <jrjuanj01@gmail.com>
2024-09-19 12:14:32 -0400

    Bulk upload

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,25 @@
+const Fs = require('fs');
+
+function countStudents(path) {
+  try {
+    let databaseFile = Fs.readFileSync(path, 'utf8').toString();
+    databaseFile = databaseFile.split('\n').filter((line) => line.trim() !== '');
+    databaseFile.shift();
+    const numOfStudents = databaseFile.length;
+    const result = [`Number of students: ${numOfStudents}`, [], []];
+    databaseFile.forEach((element) => {
+      const resultElement = element.split(',');
+      if (resultElement[3] === 'CS') {
+        result[1].push(resultElement[0]);
+      } else if (resultElement[3] === 'SWE') { result[2].push(resultElement[0]); }
+    });
+    const finalResult = `${result[0]}\nNumber of students in CS: ${result[1].length}. List: ${result[1].toString().split(',').join(', ')}\nNumber of students in SWE: ${result[2].length}. List: ${result[2].toString().split(',').join(', ')}`;
+    console.log(finalResult);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error('Cannot load the database');
+    }
+  }
+}
+
+module.exports = countStudents;
